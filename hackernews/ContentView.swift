c680cfd15aed0a3fe62bd683c234779701cdb314
Nico Cobelo <ingcobelo@gmail.com>
2020-11-14 13:23:00 -0500

    Made the title posts and their respective points visible

--- hackernews/ContentView.swift
+++ hackernews/ContentView.swift
@@ -9,12 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {
-            List(posts) { post in
-                Text(post.title)
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
             }
             .navigationBarTitle("H4CK0R NEWS")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
         }
     }
 }
@@ -25,8 +34,8 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-let posts = [
-    Post(id: "1", title: "Hello"),
-    Post(id: "2", title: "Bonjour"),
-    Post(id: "3", title: "Hola")
-]
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Bonjour"),
+//    Post(id: "3", title: "Hola")
+//]
